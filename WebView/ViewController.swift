768b5479b03c85283a6214417179cc548f0598bb
Rafinad <rafinad@outlook.com>
2021-04-30 13:39:54 +0300

    default page add

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://www.ya.ru")
+        let request = URLRequest(url: url!)
+        webView?.load(request)
+        webView?.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
     }
 
